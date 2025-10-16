bca0e305867ed0b53fa798720fb3c990a52ba832
Lynhlyn <linhctmph45529@fpt.edu.vn>
2025-10-16 13:46:18 +0700

    Initial commit

---
+++ src/test/java/com/example/nikonbe/NikonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nikonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
